d9596698398fc8cd85d598b5a452bf4d1b3adf49
itmanharry <jack_chengqian@163.com>
2023-03-23 16:10:15 +0800

    django authentication system

--- isms/settings.py
+++ isms/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os, sys
-from django.urls import path
+import os
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

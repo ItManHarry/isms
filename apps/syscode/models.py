aedc8f35526d8d39341a79d842a70c3d1122b544
itmanharry <jack_chengqian@163.com>
2023-03-30 11:01:02 +0800

    form validation

--- apps/syscode/models.py
+++ apps/syscode/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from db import BaseModel
+from django.core.exceptions import ValidationError
+def validate_wordbook_code(new_code):
+    if WordBook.objects.filter(code=new_code.upper()):
+        raise ValidationError('字典代码重复！')
 class WordBook(BaseModel):
     name = models.CharField(max_length=128)
-    code = models.CharField(max_length=4, unique=True)
+    code = models.CharField(max_length=4, unique=True, validators=[validate_wordbook_code])
 
     def __str__(self):
         return self.name
